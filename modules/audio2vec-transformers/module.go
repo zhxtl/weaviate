d204bd725edd577207c46e7db99639432986dbd8
Zain Hasan <zain_has@hotmail.com>
2023-05-16 15:34:58 -0400

    audio2vec from img2vec-neural

---
+++ modules/audio2vec-transformers/module.go
@@ -0,0 +1,107 @@
+//                           _       _
+// __      _____  __ ___   ___  __ _| |_ ___
+// \ \ /\ / / _ \/ _` \ \ / / |/ _` | __/ _ \
+//  \ V  V /  __/ (_| |\ V /| | (_| | ||  __/
+//   \_/\_/ \___|\__,_| \_/ |_|\__,_|\__\___|
+//
+//  Copyright © 2016 - 2023 Weaviate B.V. All rights reserved.
+//
+//  CONTACT: hello@weaviate.io
+//
+
+package modimage
+
+import (
+	"context"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/pkg/errors"
+	"github.com/sirupsen/logrus"
+	"github.com/weaviate/weaviate/entities/models"
+	"github.com/weaviate/weaviate/entities/modulecapabilities"
+	"github.com/weaviate/weaviate/entities/moduletools"
+	"github.com/weaviate/weaviate/modules/img2vec-neural/clients"
+	"github.com/weaviate/weaviate/modules/img2vec-neural/vectorizer"
+)
+
+func New() *ImageModule {
+	return &ImageModule{}
+}
+
+type ImageModule struct {
+	vectorizer      imageVectorizer
+	graphqlProvider modulecapabilities.GraphQLArguments
+	searcher        modulecapabilities.Searcher
+}
+
+type imageVectorizer interface {
+	Object(ctx context.Context, object *models.Object, objDiff *moduletools.ObjectDiff,
+		settings vectorizer.ClassSettings) error
+	VectorizeImage(ctx context.Context,
+		id, image string) ([]float32, error)
+}
+
+func (m *ImageModule) Name() string {
+	return "img2vec-neural"
+}
+
+func (m *ImageModule) Type() modulecapabilities.ModuleType {
+	return modulecapabilities.Img2Vec
+}
+
+func (m *ImageModule) Init(ctx context.Context,
+	params moduletools.ModuleInitParams,
+) error {
+	if err := m.initVectorizer(ctx, params.GetLogger()); err != nil {
+		return errors.Wrap(err, "init vectorizer")
+	}
+
+	if err := m.initNearImage(); err != nil {
+		return errors.Wrap(err, "init near text")
+	}
+
+	return nil
+}
+
+func (m *ImageModule) initVectorizer(ctx context.Context,
+	logger logrus.FieldLogger,
+) error {
+	// TODO: proper config management
+	uri := os.Getenv("IMAGE_INFERENCE_API")
+	if uri == "" {
+		return errors.Errorf("required variable IMAGE_INFERENCE_API is not set")
+	}
+
+	client := clients.New(uri, logger)
+	if err := client.WaitForStartup(ctx, 1*time.Second); err != nil {
+		return errors.Wrap(err, "init remote vectorizer")
+	}
+
+	m.vectorizer = vectorizer.New(client)
+
+	return nil
+}
+
+func (m *ImageModule) RootHandler() http.Handler {
+	// TODO: remove once this is a capability interface
+	return nil
+}
+
+func (m *ImageModule) VectorizeObject(ctx context.Context,
+	obj *models.Object, objDiff *moduletools.ObjectDiff, cfg moduletools.ClassConfig,
+) error {
+	icheck := vectorizer.NewClassSettings(cfg)
+	return m.vectorizer.Object(ctx, obj, objDiff, icheck)
+}
+
+func (m *ImageModule) MetaInfo() (map[string]interface{}, error) {
+	return map[string]interface{}{}, nil
+}
+
+// verify we implement the modules.Module interface
+var (
+	_ = modulecapabilities.Module(New())
+	_ = modulecapabilities.Vectorizer(New())
+)
---
+++ modules/audio2vec-transformers/vectorizer/vectorizer.go
@@ -0,0 +1,101 @@
+//                           _       _
+// __      _____  __ ___   ___  __ _| |_ ___
+// \ \ /\ / / _ \/ _` \ \ / / |/ _` | __/ _ \
+//  \ V  V /  __/ (_| |\ V /| | (_| | ||  __/
+//   \_/\_/ \___|\__,_| \_/ |_|\__,_|\__\___|
+//
+//  Copyright © 2016 - 2023 Weaviate B.V. All rights reserved.
+//
+//  CONTACT: hello@weaviate.io
+//
+
+package vectorizer
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/go-openapi/strfmt"
+	"github.com/weaviate/weaviate/entities/models"
+	"github.com/weaviate/weaviate/entities/moduletools"
+	"github.com/weaviate/weaviate/modules/img2vec-neural/ent"
+	libvectorizer "github.com/weaviate/weaviate/usecases/vectorizer"
+)
+
+type Vectorizer struct {
+	client Client
+}
+
+func New(client Client) *Vectorizer {
+	return &Vectorizer{
+		client: client,
+	}
+}
+
+type Client interface {
+	Vectorize(ctx context.Context,
+		id, image string) (*ent.VectorizationResult, error)
+}
+
+type ClassSettings interface {
+	ImageField(property string) bool
+}
+
+func (v *Vectorizer) Object(ctx context.Context, object *models.Object,
+	objDiff *moduletools.ObjectDiff, settings ClassSettings,
+) error {
+	vec, err := v.object(ctx, object.ID, object.Properties, objDiff, settings)
+	if err != nil {
+		return err
+	}
+
+	object.Vector = vec
+	return nil
+}
+
+func (v *Vectorizer) VectorizeImage(ctx context.Context, id, image string) ([]float32, error) {
+	res, err := v.client.Vectorize(ctx, id, image)
+	if err != nil {
+		return nil, err
+	}
+
+	return res.Vector, nil
+}
+
+func (v *Vectorizer) object(ctx context.Context, id strfmt.UUID,
+	schema interface{}, objDiff *moduletools.ObjectDiff, ichek ClassSettings,
+) ([]float32, error) {
+	vectorize := objDiff == nil || objDiff.GetVec() == nil
+
+	// vectorize image
+	images := []string{}
+	if schema != nil {
+		for prop, value := range schema.(map[string]interface{}) {
+			if !ichek.ImageField(prop) {
+				continue
+			}
+			valueString, ok := value.(string)
+			if ok {
+				images = append(images, valueString)
+				vectorize = vectorize || (objDiff != nil && objDiff.IsChangedProp(prop))
+			}
+		}
+	}
+
+	// no property was changed, old vector can be used
+	if !vectorize {
+		return objDiff.GetVec(), nil
+	}
+
+	vectors := [][]float32{}
+	for i, image := range images {
+		imgID := fmt.Sprintf("%s_%v", id, i)
+		vector, err := v.VectorizeImage(ctx, imgID, image)
+		if err != nil {
+			return nil, err
+		}
+		vectors = append(vectors, vector)
+	}
+
+	return libvectorizer.CombineVectors(vectors), nil
+}
