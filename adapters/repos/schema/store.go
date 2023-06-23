8b1b02c7cf4dbcba8eb48f25ae8023e0e72242c2
Redouan El Rhazouani <81578195+redouan-rhazouani@users.noreply.github.com>
2023-06-23 16:37:01 +0200

    schema:store: delete shards

--- adapters/repos/schema/store.go
+++ adapters/repos/schema/store.go
@@ -299,6 +299,18 @@ func (r *store) NewShards(_ context.Context, class string, shards []ucs.KeyValue
 	return r.db.Update(f)
 }
 
+func (r *store) DeleteShards(_ context.Context, class string, shards []string) error {
+	classKey := encodeClassName(class)
+	f := func(tx *bolt.Tx) error {
+		b := tx.Bucket(schemaBucket).Bucket(classKey)
+		if b == nil {
+			return fmt.Errorf("class not found")
+		}
+		return deleteShards(b, shards, make([]byte, 1, 68))
+	}
+	return r.db.Update(f)
+}
+
 // Load loads the complete schema from the persistent storage
 func (r *store) Load(ctx context.Context) (ucs.State, error) {
 	state := ucs.NewState(32)
@@ -454,6 +466,19 @@ func appendShards(b *bolt.Bucket, shards []ucs.KeyValuePair, key []byte) error {
 			return err
 		}
 		key = key[:1]
+	}
+	return nil
+}
+
+func deleteShards(b *bolt.Bucket, shards []string, keyBuf []byte) error {
+	keyBuf[0] = eTypeShard
+	for _, name := range shards {
+		kLen := len(name) + 1
+		keyBuf = append(keyBuf, name...)
+		if err := b.Delete(keyBuf[:kLen]); err != nil {
+			return err
+		}
+		keyBuf = keyBuf[:1]
 	}
 	return nil
 }
