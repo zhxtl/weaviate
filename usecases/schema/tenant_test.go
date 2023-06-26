bc711b19409e59dd73967479ee2cd90a3135ce27
Redouan El Rhazouani <81578195+redouan-rhazouani@users.noreply.github.com>
2023-06-26 14:27:15 +0200

    schema: unit test remove tenants

--- usecases/schema/tenant_test.go
+++ usecases/schema/tenant_test.go
@@ -48,9 +48,10 @@ func TestAddTenants(t *testing.T) {
 			Class:   "UnknownClass",
 			tenants: tenants,
 			initial: &models.Class{
-				Class: cls, MultiTenancyConfig: mt,
-				Properties:        properties,
-				ReplicationConfig: repConfig,
+				Class:              cls,
+				MultiTenancyConfig: mt,
+				Properties:         properties,
+				ReplicationConfig:  repConfig,
 			},
 			errMsg: ErrNotFound.Error(),
 		},
@@ -105,18 +106,141 @@ func TestAddTenants(t *testing.T) {
 		// TODO test with replication factor >= 2
 	}
 
+	// AddTenants
 	for _, test := range tests {
-		t.Run(test.name, func(t *testing.T) {
-			sm := newSchemaManager()
-			err := sm.AddClass(ctx, nil, test.initial)
-			if err == nil {
-				err = sm.AddTenants(ctx, nil, test.Class, test.tenants)
+		sm := newSchemaManager()
+		err := sm.AddClass(ctx, nil, test.initial)
+		if err == nil {
+			err = sm.AddTenants(ctx, nil, test.Class, test.tenants)
+		}
+		if test.errMsg == "" {
+			assert.Nil(t, err)
+			ss := sm.state.ShardingState[test.Class]
+			assert.NotNil(t, ss, test.name)
+			assert.Equal(t, len(ss.Physical), len(test.tenants), test.name)
+		} else {
+			assert.ErrorContains(t, err, test.errMsg, test.name)
+		}
+
+	}
+}
+
+func TestRemoveTenants(t *testing.T) {
+	var (
+		ctx     = context.Background()
+		mt      = &models.MultiTenancyConfig{Enabled: true, TenantKey: "uUID"}
+		tenants = []*models.Tenant{
+			{Name: "USER1"},
+			{Name: "USER2"},
+			{Name: "USER3"},
+			{Name: "USER4"},
+		}
+		cls        = "C1"
+		properties = []*models.Property{
+			{
+				Name:     "uUID",
+				DataType: schema.DataTypeText.PropString(),
+			},
+		}
+		repConfig = &models.ReplicationConfig{Factor: 1}
+	)
+
+	type test struct {
+		name       string
+		Class      string
+		tenants    []*models.Tenant
+		initial    *models.Class
+		errMsg     string
+		addTenants bool
+	}
+	tests := []test{
+		{
+			name:    "UnknownClass",
+			Class:   "UnknownClass",
+			tenants: tenants,
+			initial: &models.Class{
+				Class: cls, MultiTenancyConfig: mt,
+				Properties:        properties,
+				ReplicationConfig: repConfig,
+			},
+			errMsg: ErrNotFound.Error(),
+		},
+		{
+			name:    "MTIsNil",
+			Class:   "C1",
+			tenants: tenants,
+			initial: &models.Class{
+				Class:              cls,
+				MultiTenancyConfig: nil,
+				Properties:         properties,
+				ReplicationConfig:  repConfig,
+			},
+			errMsg: "not enabled",
+		},
+		{
+			name:    "MTDisabled",
+			Class:   "C1",
+			tenants: tenants,
+			initial: &models.Class{
+				Class:              cls,
+				MultiTenancyConfig: &models.MultiTenancyConfig{Enabled: false, TenantKey: "uUID"},
+				Properties:         properties,
+				ReplicationConfig:  repConfig,
+			},
+			errMsg: "not enabled",
+		},
+		{
+			name:    "EmptyTenantKeyValue",
+			Class:   "C1",
+			tenants: []*models.Tenant{{"A"}, {""}, {"B"}},
+			initial: &models.Class{
+				Class:              cls,
+				MultiTenancyConfig: &models.MultiTenancyConfig{Enabled: true, TenantKey: "uUID"},
+				Properties:         properties,
+				ReplicationConfig:  repConfig,
+			},
+			errMsg: "tenant",
+		},
+		{
+			name:    "Success",
+			Class:   "C1",
+			tenants: tenants[:2],
+			initial: &models.Class{
+				Class:              cls,
+				MultiTenancyConfig: &models.MultiTenancyConfig{Enabled: true, TenantKey: "uUID"},
+				Properties:         properties,
+				ReplicationConfig:  repConfig,
+			},
+			errMsg:     "",
+			addTenants: true,
+		},
+	}
+
+	for _, test := range tests {
+		sm := newSchemaManager()
+		err := sm.AddClass(ctx, nil, test.initial)
+		if err != nil {
+			t.Fatalf("%s: add class: %v", test.name, err)
+		}
+		if test.addTenants {
+			err = sm.AddTenants(ctx, nil, test.Class, tenants)
+			if err != nil {
+				t.Fatalf("%s: add tenants: %v", test.name, err)
 			}
-			if test.errMsg == "" {
-				assert.Nil(t, err)
-			} else {
-				assert.ErrorContains(t, err, test.errMsg)
+		}
+		err = sm.RemoveTenants(ctx, nil, test.Class, test.tenants)
+		if test.errMsg == "" {
+			if err != nil {
+				t.Fatalf("%s: remove tenants: %v", test.name, err)
 			}
-		})
+			ss := sm.state.ShardingState[test.Class]
+			if ss == nil {
+				t.Fatalf("%s: sharding state equal nil", test.name)
+			}
+			assert.Equal(t, len(test.tenants)+len(ss.Physical), len(tenants))
+		} else {
+			assert.ErrorContains(t, err, test.errMsg, test.name)
+		}
+
 	}
 }
