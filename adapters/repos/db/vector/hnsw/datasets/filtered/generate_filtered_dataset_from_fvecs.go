9ff09f3ec8012affd24bc7f6be8bf04831ac7a9f
Connor Shorten <connorshorten300@gmail.com>
2024-02-01 19:27:52 -0500

    Clean up comments

--- adapters/repos/db/vector/hnsw/datasets/filtered/generate_filtered_dataset_from_fvecs.go
+++ adapters/repos/db/vector/hnsw/datasets/filtered/generate_filtered_dataset_from_fvecs.go
@@ -38,16 +38,12 @@ type GroundTruth struct {
 }
 
 func main() {
-	// CLI flags
-	//filePath := flag.String("DataPath", "./sift-data/sift_base.fvecs", "Path to the data file")
+	// CLI flags, ToDo: Add `filePath` and `vectorDimension` to CLI
 	numVectors := flag.Int("numVectors", 100_000, "Number of vectors to process")
 	majorityPct := flag.Float64("majorityPct", 95.0, "Minority filter percentage of the dataset")
-	//vectorDimension := flag.Int("vectorDim", 128, "Dimension of the vectors")
 
-	// parse the flags
 	flag.Parse()
 
-	// Read base vectors from file
 	vectors := ReadSiftVecsFrom("./sift-data/sift_base.fvecs", *numVectors, 128)
 
 	saveIndexVectors := make([]Vector, len(vectors))
@@ -88,14 +84,10 @@ func main() {
 	saveIndexFiltersJSON, _ := json.Marshal(saveIndexFilters)
 	ioutil.WriteFile(index_with_filters_save_path, saveIndexFiltersJSON, 0o644)
 
-	// Read the query vectors from files
-	_, queryVectors := ReadVecs(*numVectors, 10_000, 128, "sift") // probably should separate ReadVecs into two separate calls lmao
+	_, queryVectors := ReadVecs(*numVectors, 10_000, 128, "sift")
 
 	saveQueryVectors := make([]Vector, len(queryVectors))
 	saveQueryFilters := make([]Filters, len(queryVectors))
-
-	//golang doesn't like this - race condition: var queryFilter map[int]int
-	// For each query vector, perform search and calculate recall
 
 	groundTruths := make([]GroundTruth, len(queryVectors))
 
@@ -155,15 +147,13 @@ func main() {
 	saveQueryFiltersJSON, _ := json.Marshal(saveQueryFilters)
 	ioutil.WriteFile(query_vectors_with_filters_save_path, saveQueryFiltersJSON, 0o644)
 
-	// Save all nearest neighbors to a JSON file
 	saveGroundTruthsJSON, _ := json.Marshal(groundTruths)
 	ground_truth_save_path := "filtered-recall-truths-" + strconv.Itoa(*numVectors) + ".json"
 	ioutil.WriteFile(ground_truth_save_path, saveGroundTruthsJSON, 0o644)
 
-	fmt.Println("Finished.\n")
+	fmt.Printf("Finished.\n")
 }
 
-// Function to calculate nearest neighbors of a vector using brute force
 func calculateNearestNeighborsWithFilters(query []float32, indexVectors []VecWithFilters, numNeighbors int, queryFilters map[int]int) []int {
 	filteredIndex := make([]VecWithFilters, 0)
 	for _, v := range indexVectors {
@@ -185,7 +175,6 @@ func calculateNearestNeighborsWithFilters(query []float32, indexVectors []VecWit
 	}
 	distances := make([]DistanceIndex, len(filteredIndex))
 
-	// Compute the distance from the query to each vector
 	for i, v := range filteredIndex {
 		distances[i] = DistanceIndex{
 			Distance: BFeuclideanDistance(query, v.Vector),
@@ -193,12 +182,10 @@ func calculateNearestNeighborsWithFilters(query []float32, indexVectors []VecWit
 		}
 	}
 
-	// Sort the distances
 	sort.Slice(distances, func(i, j int) bool {
 		return distances[i].Distance < distances[j].Distance
 	})
 
-	// Extract the indices of the numNeighbors nearest neighbors
 	neighbors := make([]int, numNeighbors)
 
 	i := 0
@@ -210,7 +197,6 @@ func calculateNearestNeighborsWithFilters(query []float32, indexVectors []VecWit
 	return neighbors
 }
 
-// Function to calculate the Euclidean distance between two vectors
 func BFeuclideanDistance(a, b []float32) float32 {
 	var sum float32
 	for i := range a {
@@ -220,7 +206,6 @@ func BFeuclideanDistance(a, b []float32) float32 {
 	return float32(math.Sqrt(float64(sum)))
 }
 
-// Function to write integer vectors to a SIFT formatted binary file
 func writeSiftIVecsToFile(filename string, vectors [][]int) error {
 	file, err := os.Create(filename)
 	if err != nil {
@@ -239,18 +224,15 @@ func writeSiftIVecsToFile(filename string, vectors [][]int) error {
 }
 
 func writeSiftInt(w io.Writer, vector []int) error {
-	// Convert vector to []int32
 	vectorInt32 := make([]int32, len(vector))
 	for i, v := range vector {
 		vectorInt32[i] = int32(v)
 	}
 
-	// Write vector dimension
 	if err := binary.Write(w, binary.LittleEndian, int32(len(vectorInt32))); err != nil {
 		return err
 	}
 
-	// Write vector
 	if err := binary.Write(w, binary.LittleEndian, vectorInt32); err != nil {
 		return err
 	}
@@ -258,106 +240,79 @@ func writeSiftInt(w io.Writer, vector []int) error {
 	return nil
 }
 
-// Function to read SIFT formatted vector data from a given path
 func ReadSiftVecsFrom(path string, size int, dimensions int) [][]float32 {
-	// print progress
 	fmt.Printf("generating %d vectors...", size)
 
-	// read the vectors
 	vectors := readSiftFloat(path, size, dimensions)
 
-	// print completion
 	fmt.Printf(" done\n")
 
-	// return the vectors
 	return vectors
 }
 
-// Function to read base and query vector data from a given path
 func ReadVecs(size int, queriesSize int, dimensions int, db string, path ...string) ([][]float32, [][]float32) {
-	// print progress
 	fmt.Printf("generating %d vectors...", size+queriesSize)
 
-	// set the base uri as db
 	uri := db
 
-	// if a path is provided, prepend it to uri
 	if len(path) > 0 {
 		uri = fmt.Sprintf("%s/%s", path[0], uri)
 	}
 
-	// read base vectors
 	vectors := readSiftFloat(fmt.Sprintf("sift-data/%s_base.fvecs", db), size, dimensions)
 
-	// read query vectors
 	queries := readSiftFloat(fmt.Sprintf("sift-data/%s_query.fvecs", db), queriesSize, dimensions)
 
-	// print completion
 	fmt.Printf(" done\n")
 
-	// return vectors and queries
 	return vectors, queries
 }
 
-// Function to read SIFT formatted vector data from a given binary file
 func readSiftFloat(file string, maxObjects int, vectorLengthFloat int) [][]float32 {
-	// open the file
 	f, err := os.Open(file)
 
-	// ensure file gets closed after the function exits
 	defer f.Close()
 
-	// check for file open error
 	if err != nil {
 		panic(err)
 	}
 
-	// Allocate memory for objects and vectorBytes
 	objects := make([][]float32, maxObjects)
 	vectorBytes := make([]byte, 4+vectorLengthFloat*4)
 
-	// read the vectors from the file
 	for i := 0; i >= 0; i++ {
 		_, err = f.Read(vectorBytes)
 
-		// break the loop if we have reached end of file
 		if err == io.EOF {
 			break
 		} else if err != nil {
 			panic(err)
 		}
 
-		// check if the vector length matches expected length
 		if int32FromBytes(vectorBytes[0:4]) != vectorLengthFloat {
 			panic("Each vector must have 128 entries.")
 		}
 
-		// read each float from the vector
 		vectorFloat := make([]float32, vectorLengthFloat)
 		for j := 0; j < vectorLengthFloat; j++ {
 			start := (j + 1) * 4 // first 4 bytes are length of vector
 			vectorFloat[j] = float32FromBytes(vectorBytes[start : start+4])
 		}
 
-		// save the vector
 		objects[i] = vectorFloat
 
-		// break the loop if we have reached maximum number of objects
 		if i >= maxObjects-1 {
 			break
 		}
 	}
 
-	// return the objects read from file
 	return objects
 }
 
-// Function to convert a byte slice to int
 func int32FromBytes(bytes []byte) int {
 	return int(binary.LittleEndian.Uint32(bytes))
 }
 
-// Function to convert a byte slice to float32
 func float32FromBytes(bytes []byte) float32 {
 	bits := binary.LittleEndian.Uint32(bytes)
 	float := math.Float32frombits(bits)
