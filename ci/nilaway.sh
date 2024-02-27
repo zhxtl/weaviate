c26e325f0bc04fc58f9a1af40a1ac4c940a01748
moogacs <contact@m-mooga.com>
2024-02-27 10:26:18 +0100

    more

--- ci/nilaway.sh
+++ ci/nilaway.sh
@@ -1,7 +1,4 @@
 git clone https://github.com/moogacs/nilaway-plugin.git
 cd nilaway-plugin
-go mod init github.com/nilaway-plugin
-go mod tidy
-go version -m $(which golangci-lint)
-go build -o '${{ github.workspace }}/.plugins/nilaway.so'  -buildmode=plugin plugin/nilaway.go          
-echo ${{ github.workspace }}
+chmod +x build.sh
+./build.sh
